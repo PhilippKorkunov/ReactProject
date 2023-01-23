7971725e4506d0148b8194d07ca451468fef7dca
Philipp <filppkorkunov.1920@gmail.com>
2023-01-23 14:05:59 +0300

    Добавьте файлы проекта.

---
+++ Controllers/WebAppController.cs
@@ -0,0 +1,37 @@
+﻿using DataBaseLibrary;
+using DataBaseLibrary.Entyties;
+using DataBaseLibrary.PostgreSQLProcessing;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using ReactProject.Models;
+
+namespace ReactProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebAppController : ControllerBase
+    {
+        private static readonly Repository<Order> repositoryOrder;
+        static WebAppController()
+        {
+            var eFDBContext= new EFDBContext();
+            repositoryOrder = new Repository<Order>(eFDBContext);
+        }
+
+
+        [HttpGet("GetAllOrders")]
+        public async Task<IActionResult> GetAllOrdersAsync() => Ok(Converter.ToNewOrderList(await repositoryOrder.GetAsync()));
+
+        [HttpGet("GetOrder")]
+        public async Task<IActionResult> GetOrderAsync(int id) => Ok(Converter.ToNewOrderList(await repositoryOrder.GetAsync(id: id)));
+
+        [HttpPost("AddOrder")]
+        public async Task<IActionResult> AddOrderAsync(JsonDocument jsonDocument)
+        {
+            await repositoryOrder.AddAsync(new Converter(jsonDocument).ToOrder());
+            return Ok();
+        }
+
+
+    }
+}
